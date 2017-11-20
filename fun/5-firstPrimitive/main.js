e020aa7055627721724293cabec05c9cdba678ae
OhBonsai <letbonsaibe@gmail.com>
2017-11-20 22:34:43 +0800

    update first Primitive

---
+++ fun/5-firstPrimitive/main.js
@@ -0,0 +1,59 @@
+const fs = `#version 300 es
+    precision mediump float;
+
+    in lowp vec4 color;
+    out vec4 finalColor;
+    
+    void main(void){
+        finalColor = color;
+    }
+`;
+
+
+// layout是3.0的语法，提前预设好uniform的位置。以前都是getUniformLocation(xx)，返回一个值。
+// 不过好像getUniformLocation也是按照你代码顺序，uniformLocation从1慢慢排的
+const vs = `#version 300 es
+    in vec3 a_position;
+    layout(location=5) in float a_color;
+    
+    uniform vec3 uColor[4];
+    
+    out lowp vec4 color;
+
+    void main(void){
+        color = vec4(uColor[int(a_color)], 1.0);
+        gl_Position = vec4(a_position, 1.0 );
+    }
+`;
+
+let gl,
+    renderLoop,
+    gShader = null,
+    gModel = null;
+
+
+class TestShader extends Shader {
+    constructor(gl, aryColor) {
+        super(gl, vs, fs);
+
+        this.uniformLoc.uColor = gl.getUniformLocation(this.program, "uColor");
+        gl.uniform3fv(this.uniformLoc.uColor, new Float32Array(aryColor));
+        gl.useProgram(null);
+    }
+}
+
+let onRender = function () {
+    gl.fClear();
+    gShader.activate().renderModel(gModel);
+};
+
+function main() {
+    gl = glInstance('container').fClear().fSetSize(400, 400);
+    gShader = new TestShader(gl, [.8, .8, .8, 1, 0, 0, 0, 1, 0, 0, 0, 1]);// Grey Red Green Blue
+    gModel = new Model(Primitive.GridAxis.createGrid(gl));
+    renderLoop = new RenderLoop(onRender).start();
+}
+
+main();
+
+
---
+++ fun/5-firstPrimitive/shader.js
@@ -0,0 +1,104 @@
+class Shader {
+    constructor(gl, vS, fS){
+        this.program = ShaderUtil.createProgram(gl, vS, fS, true);
+
+        if(this.program !== null){
+            this.gl = gl;
+            gl.useProgram(this.program);
+            this.attribLoc = ShaderUtil.getShaderAttribLocation(gl, this.program);
+            this.uniformLoc = {
+
+            }
+        }
+    }
+
+    activate() {
+        this.gl.useProgram(this.program);
+        return this;
+    }
+
+    deactivate(){
+        this.gl.useProgram(null);
+        return this;
+    }
+
+    dispose(){
+        if(this.gl.getParameter(this.gl.CURRENT_PROGRAM) === this.program){
+            this.gl.useProgram(null);
+        }
+        this.gl.deleteProgram(this.program);
+    }
+
+    preRender(){}
+
+    renderModel(model){
+        this.gl.bindVertexArray(model.mesh.vao);
+        if(model.mesh.indexCount){
+            this.gl.drawElements(model.mesh.drawMode, model.mesh.indexLength, gl.UNSIGNED_SHORT, 0);
+        }else{
+                this.gl.drawArrays(model.mesh.drawMode, 0, model.mesh.vertexCount);
+        }
+
+        this.gl.bindVertexArray(null);
+        return this;
+    }
+}
+
+
+class ShaderUtil {
+    static createShader (gl, str, type){
+        let shader = gl.createShader(type);
+        gl.shaderSource(shader, str);
+        gl.compileShader(shader);
+
+        if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+            alert(gl.getShaderInfoLog(shader));
+            gl.deleteShader(shader);
+            return null
+        }
+
+        return shader
+    }
+
+    static createProgram (gl, vs, fs, doValidate) {
+        let vShader = ShaderUtil.createShader(gl, vs, gl.VERTEX_SHADER);
+        let fShader = ShaderUtil.createShader(gl, fs, gl.FRAGMENT_SHADER);
+
+        let program = gl.createProgram();
+        gl.attachShader(program, vShader);
+        gl.attachShader(program, fShader);
+        gl.bindAttribLocation(program, ATTR_POSTION_LOC, ATTR_POSITION_NAME);
+        gl.bindAttribLocation(program, ATTR_NORMAL_LOC, ATTR_NORMAL_NAME);
+        gl.bindAttribLocation(program, ATTR_UV_LOC, ATTR_UV_NAME);
+
+
+
+        gl.linkProgram(program);
+
+        if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
+            alert(gl.getProgramInfoLog(program));
+            gl.deleteProgram(program);
+            return null
+        }
+
+        if(doValidate){
+            gl.validateProgram(program);
+            if(!gl.getProgramParameter(program, gl.VALIDATE_STATUS)){
+                alert((gl.getProgramInfoLog(program)));
+                gl.deleteProgram(program);
+                return null
+            }
+        }
+
+        return program
+    }
+
+    static getShaderAttribLocation(gl, program){
+        return {
+            position:   gl.getAttribLocation(program, ATTR_POSITION_NAME),
+            normal:     gl.getAttribLocation(program, ATTR_NORMAL_NAME),
+            uv:         gl.getAttribLocation(program, ATTR_UV_NAME),
+        }
+    }
+
+}
