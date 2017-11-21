6ae8457c4cde9946d35d22a104c493ccbe5f666e
游剑涛 <youjiantao@gridsum.com>
2017-11-21 22:16:04 +0800

    update first transform

---
+++ fun/6-firstTransform/transform.js
@@ -0,0 +1,48 @@
+class Transform{
+
+    constructor(){
+        this.position   = new Vector3(0., 0., 0.);
+        this.scale      = new Vector3(1., 1., 1.);
+        this.rotation   = new Vector3(0., 0., 0.);
+        this.matView    = new Matrix4();
+        this.matNormal  = new Float32Array(9);
+
+        this.forward    = new Float32Array(4);
+        this.up         = new Float32Array(4);
+        this.right      = new Float32Array(4);
+    }
+
+    updateMatrix(){
+        this.matView.reset()
+            .vtranslate(this.position)
+            .rotateX(this.rotation.x * Transform.deg2Rad)
+            .rotateZ(this.rotation.z * Transform.deg2Rad)
+            .rotateY(this.rotation.y * Transform.deg2Rad)
+            .vscale(this.scale);
+
+        Matrix4.normalMat3(this.matNormal, this.matView.raw);
+
+        Matrix4.transformVec4(this.forward,	[0,0,1,0],this.matView.raw);
+        Matrix4.transformVec4(this.up,		[0,1,0,0],this.matView.raw);
+        Matrix4.transformVec4(this.right,	[1,0,0,0],this.matView.raw);
+
+        return this.matView.raw;
+    }
+
+    getViewMatrix(){
+        return this.matView.raw
+    }
+
+    getNormalMatrix(){
+        return this.matNormal;
+    }
+
+    reset(){
+        this.position.set(0., 0., 0.);
+        this.scale.set(1., 1., 1.);
+        this.rotation.set(0., 0., 0.);
+    }
+
+}
+
+Transform.deg2Rad = Math.PI/180;
